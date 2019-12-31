560f1c651d9237de623187cf272839ece8dd7f51
KONOS-PC1 <kjmc4114@naver.com>
2019-12-31 14:33:08 +0900

    insta_clone

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -6,16 +6,31 @@ class Tabpage extends StatefulWidget {
 }
 
 class _TabpageState extends State<Tabpage> {
+  int _selectedIndex = 0;
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-         items: <BottomNavigationBarItem>[
+        body: Center(child:_pages[_selectedIndex]),
+        bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex, //current index = 현재 페이지의 index 와 같다
+        items: <BottomNavigationBarItem>[
            BottomNavigationBarItem(icon: Icon(Icons.home) ,title: Text('home')),
            BottomNavigationBarItem(icon: Icon(Icons.search) ,title: Text('찾기')),
            BottomNavigationBarItem(icon: Icon(Icons.account_box) ,title: Text('내정보')),
          ]
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
