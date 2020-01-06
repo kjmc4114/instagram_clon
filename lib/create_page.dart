aae6a0b4fd64788bd5aefb05ff6d54a7b7a5c253
KONOS-PC1 <kjmc4114@naver.com>
2020-01-06 16:37:34 +0900

    insta_clone

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -7,6 +10,8 @@ class CreatePage extends StatefulWidget {
 
 class _CreatePageState extends State<CreatePage> {
   final textEditinControllr = TextEditingController();
+  File _image;
+
   @override
   void dispose() { // 메모리 해제 메소드
     textEditinControllr.dispose(); //화면이 소멸될때는 이 함수가 호출된다.
@@ -18,7 +23,10 @@ class _CreatePageState extends State<CreatePage> {
     return Scaffold(
       appBar: _buildAppBar(),
       body: _bulidBody(),
-      floatingActionButton: FloatingActionButton(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+        onPressed: _getImage,
+      ),
     );
   }
 
@@ -27,24 +35,33 @@ class _CreatePageState extends State<CreatePage> {
       actions: <Widget>[
          IconButton(
            icon: Icon(Icons.add),
-           onPressed: (){
-
-           },
+           onPressed: null,
          )
       ],
     );
   }
 
   Widget _bulidBody(){
-    return Column(
-      children: <Widget>[
-        Text('noImage'),
-        TextField(
-          decoration: InputDecoration(hintText: '내용을 입력하세요'),
-          controller: textEditinControllr,
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _image == null ? Text('noImage') : Image.file(_image),
+          TextField(
+            decoration: InputDecoration(hintText: '내용을 입력하세요'),
+            controller: textEditinControllr,
+          )
+        ],
+      ),
     );
+  }
+  // Future <File> 은 Return 이  Future, 그리고 변수는 File로 설정해 준다.
+  Future _getImage() async { // 비동기식처리
+   File image = await ImagePicker.pickImage(source: ImageSource.gallery);
+   // 비동기 처리는 Future 미래에 File이 될 변수임.
+
+   setState(() {
+     _image = image;
+   });
   }
 
 }
